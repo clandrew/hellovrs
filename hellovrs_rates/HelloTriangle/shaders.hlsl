e5da3135fe702a0a3a76eeb9af5c8c97636ceaae
Claire Andrews <clandrew@microsoft.com>
2021-07-12 20:07:53 -0700

    Use shading rate + tess, dummy impl

--- hellovrs_rates/HelloTriangle/shaders.hlsl
+++ hellovrs_rates/HelloTriangle/shaders.hlsl
@@ -13,6 +13,7 @@ struct PSInput
 {
 	float4 position : SV_POSITION;
 	float4 color : COLOR;
+	uint shadingRate : SV_SHADINGRATE;
 };
 
 PSInput VSMain(float4 position : POSITION, float4 color : COLOR)
@@ -21,15 +22,48 @@ PSInput VSMain(float4 position : POSITION, float4 color : COLOR)
 
 	result.position = position;
 	result.color = color;
+	result.shadingRate = 0;
+
 
 	return result;
 }
 
-float4 PSMain(PSInput input) : SV_TARGET
-{ 
-	float4 color = float4(sin(input.position.x / 10.0f), sin(input.position.y / 10.0f), cos((input.position.x + input.position.y) / 10.0f), 1);
+float4 PickShadingRateColor(uint shadingRate)
+{
+	float4 col = float4(0, 0, 0, 0);
+	switch (shadingRate)
+	{
+	case 0x0: // fine shading
+		col = float4(1, 0, 0, 1); // Red
+		break;
+	case 0x1: // 1X2
+		col = float4(1, 0.3f, 0.3f, 1); // Light red
+		break;
+	case 0x4: // 2X1
+		col = float4(0.5f, 0, 0, 1); // Dark red
+		break;
+	case 0x5: // 2x2
+		col = float4(0, 0, 1, 1); // Blue
+		break;
+	case 0x6: // 2x4
+		col = float4(0.3f, 1, 1, 1); // Light cyan
+		break;
+	case 0x9: // 4x2
+		col = float4(0, 0.5f, 0.5f, 1); // Dark cyan
+		break;
+	case 0xA: // 4x4
+		col = float4(0, 1, 0, 1); // Green
+		break;
+	default:
+		col = float4(1, 0, 1, 1); // Magenta
+		break;
+	}
+	return col;
+}
 
-	return color;
+float4 PSMain(PSInput input) : SV_TARGET
+{
+	return PickShadingRateColor(input.shadingRate);
 }
 
 struct PatchConstants
@@ -53,8 +87,10 @@ PatchConstants hs_patch(InputPatch<PSInput, 3> patch)
 PSInput HSMain(InputPatch<PSInput, 3> patch, uint cpid : SV_OutputControlPointID)
 {
 	PSInput result;
-	result.position = float4(0, 0, 0, 0);
+	result.position = patch[0].position;
 	result.color = float4(0, 0, 0, 0);
+	result.shadingRate = patch[0].shadingRate;
+
 	return result;
 }
 
@@ -62,7 +98,9 @@ PSInput HSMain(InputPatch<PSInput, 3> patch, uint cpid : SV_OutputControlPointID
 PSInput DSMain(OutputPatch<PSInput, 3> patch, PatchConstants patchConst, float3 location : SV_DomainLocation)
 {
 	PSInput result;
-	result.position = float4(0, 0, 0, 0);
+	result.position = patch[0].position;
 	result.color = float4(0, 0, 0, 0);
+	result.shadingRate = patch[0].shadingRate;
+
 	return result;
 }
