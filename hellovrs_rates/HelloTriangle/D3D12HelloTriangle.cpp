e77faf0d26ef8a2f02ead222766db6db41550447
Claire Andrews <clandrew@microsoft.com>
2021-07-12 17:32:35 -0700

    Use sm 6_4 and draw shading rate as color

--- hellovrs_rates/HelloTriangle/D3D12HelloTriangle.cpp
+++ hellovrs_rates/HelloTriangle/D3D12HelloTriangle.cpp
@@ -139,20 +139,52 @@ void
 CompileAndCheckErrors(_In_ LPCWSTR pFileName,
     _In_reads_opt_(_Inexpressible_(pDefines->Name != NULL)) CONST D3D_SHADER_MACRO* pDefines,
     _In_opt_ ID3DInclude* pInclude,
-    _In_ LPCSTR pEntrypoint,
-    _In_ LPCSTR pTarget,
+    _In_ LPCWSTR pEntrypoint,
+    _In_ LPCWSTR pTarget,
     _In_ UINT Flags1,
     _In_ UINT Flags2,
-    _Out_ ID3DBlob** ppCode)
+    _Out_ IDxcBlob** ppCode)
 {
-    ComPtr<ID3DBlob> errorMsgs;
-    HRESULT hr = D3DCompileFromFile(pFileName, pDefines, pInclude, pEntrypoint, pTarget, Flags1, Flags2, ppCode, &errorMsgs);
-    if (FAILED(hr))
-    {
-        char const* errorMsg = reinterpret_cast<char*>(errorMsgs->GetBufferPointer());
-        MessageBoxA(nullptr, errorMsg, "Failed to compile shader", MB_OK);
-        ThrowIfFailed(hr);
-    }
+	ComPtr<IDxcLibrary> library;
+	ThrowIfFailed(DxcCreateInstance(CLSID_DxcLibrary, IID_PPV_ARGS(&library)));
+
+	ComPtr<IDxcCompiler> compiler;
+	ThrowIfFailed(DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&compiler)));
+
+	uint32_t codePage = CP_UTF8;
+	ComPtr<IDxcBlobEncoding> sourceBlob;
+	ThrowIfFailed(library->CreateBlobFromFile(pFileName, &codePage, &sourceBlob));
+
+	ComPtr<IDxcOperationResult> result;
+	ThrowIfFailed(compiler->Compile(
+		sourceBlob.Get(), // pSource
+		pFileName, // pSourceName
+		pEntrypoint, // pEntryPoint
+		pTarget, // pTargetProfile
+		NULL, 0, // pArguments, argCount
+		NULL, 0, // pDefines, defineCount
+		NULL, // pIncludeHandler
+		&result)); // ppResult
+
+	HRESULT hr;
+	ThrowIfFailed(result->GetStatus(&hr));
+
+	if (FAILED(hr) && result)
+	{
+		ComPtr<IDxcBlobEncoding> errorsBlob;
+		hr = result->GetErrorBuffer(&errorsBlob);
+		if (SUCCEEDED(hr) && errorsBlob)
+		{
+			char const* errorMsg = reinterpret_cast<char*>(errorsBlob->GetBufferPointer());
+			MessageBoxA(nullptr, errorMsg, "Failed to compile shader", MB_OK);
+			return;
+		}
+	}
+
+	if (result)
+	{
+		result->GetResult(ppCode);
+	}
 }
 
 void D3D12HelloTriangle::AddQuad(float left, float bottom, float quadSize)
@@ -185,18 +217,18 @@ void D3D12HelloTriangle::LoadAssets()
 
 	// Create the pipeline state, which includes compiling and loading shaders.
 	{
-		ComPtr<ID3DBlob> vertexShader;
-		ComPtr<ID3DBlob> pixelShader;
+		ComPtr<IDxcBlob> vertexShader;
+		ComPtr<IDxcBlob> pixelShader;
 
 #if defined(_DEBUG)
 		// Enable better shader debugging with the graphics debugging tools.
-		UINT compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+		UINT compileFlags = 0;
 #else
 		UINT compileFlags = 0;
 #endif
 
-        CompileAndCheckErrors(GetAssetFullPath(L"shaders.hlsl").c_str(), nullptr, nullptr, "VSMain", "vs_5_0", compileFlags, 0, &vertexShader);
-        CompileAndCheckErrors(GetAssetFullPath(L"shaders.hlsl").c_str(), nullptr, nullptr, "PSMain", "ps_5_0", compileFlags, 0, &pixelShader);
+        CompileAndCheckErrors(GetAssetFullPath(L"shaders.hlsl").c_str(), nullptr, nullptr, L"VSMain", L"vs_6_4", compileFlags, 0, &vertexShader);
+        CompileAndCheckErrors(GetAssetFullPath(L"shaders.hlsl").c_str(), nullptr, nullptr, L"PSMain", L"ps_6_4", compileFlags, 0, &pixelShader);
 
 		// Define the vertex input layout.
 		D3D12_INPUT_ELEMENT_DESC inputElementDescs[] =
@@ -209,8 +241,8 @@ void D3D12HelloTriangle::LoadAssets()
 		D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc = {};
 		psoDesc.InputLayout = { inputElementDescs, _countof(inputElementDescs) };
 		psoDesc.pRootSignature = m_rootSignature.Get();
-		psoDesc.VS = CD3DX12_SHADER_BYTECODE(vertexShader.Get());
-		psoDesc.PS = CD3DX12_SHADER_BYTECODE(pixelShader.Get());
+		psoDesc.VS = CD3DX12_SHADER_BYTECODE(vertexShader->GetBufferPointer(), vertexShader->GetBufferSize());
+		psoDesc.PS = CD3DX12_SHADER_BYTECODE(pixelShader->GetBufferPointer(), pixelShader->GetBufferSize());
 		psoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
 		psoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
 		psoDesc.DepthStencilState.DepthEnable = FALSE;
--- hellovrs_rates/HelloTriangle/stdafx.h
+++ hellovrs_rates/HelloTriangle/stdafx.h
@@ -23,7 +23,7 @@
 
 #include <d3d12.h>
 #include <dxgi1_4.h>
-#include <D3Dcompiler.h>
+#include <dxcapi.h>
 #include <DirectXMath.h>
 #include "d3dx12.h"
 
